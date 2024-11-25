804c6e5531937e85f75ee8817bd62e68887bbdad
Luke Sorrenti <lrsorrenti@hotmail.com>
2024-11-25 10:10:58 -0600

    Added more to the final project webpage, classes, general html skeleton

--- 09_Final/index.php
+++ 09_Final/index.php
@@ -12,6 +12,44 @@
     <div class="bg-blue-500 text-white p-2 max-w-xl flex text-center mx-auto">
         <h1 class="text-3xl font-bold text-center mx-auto">Hello, Tailwind!</h1>
     </div>
+    <div>
+        <!-- Wrapper -->
+        <div>
+            <!-- Banner -->
+        </div>
+        <div>
+            <!--- Nav -->
+        </div>
+        <div>
+            <!-- Content -->
+            <div class="flex max-w-2xl justify-center gap-20 pt-4 pb-4">
+                <!-- Footer -->
+                <div class=" flex flex-col">
+                    <!-- Three Columns-->
+                    <strong>Phone</strong>
+                    555-123-4567
+                </div>
+                <div class=" flex flex-col">
+                    <!-- Three Columns-->
+                    <strong>Location</strong>
+                    123 Main St <br>
+                    New York, NY
+                </div>
+                <div class=" flex flex-col gap-0">
+                    <strong>Hours</strong>
+                    <em class="p-0 m-0">Sunday - Thursday</em>
+                    <em>11AM - 10PM</em>
+                    <em>Friday - Saturday</em>
+                    <em>11AM - 12PM</em>
+                    <!-- Three Columns-->
+                </div>
+            </div>
+        </div>
+    </div>
+    <div>
+        <!-- Copyright info -->
+        <?php include('./copyright.php') ?>
+    </div>
 </body>
 
 </html>
